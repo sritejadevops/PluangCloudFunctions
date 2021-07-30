b284afc803352384fd569b6f7a1c512c5122e246
dkm17113034 <malavdkm2@gmail.com>
2021-07-30 11:07:31 +0530

    added schema for each table in bigquery

--- src/functions/clevertap-bq-etl/main.py
+++ src/functions/clevertap-bq-etl/main.py
@@ -1,31 +1,32 @@
 import json
 import re
 from google.cloud import bigquery
-from google.cloud import  error_reporting
+from google.cloud import error_reporting
 
 
 BQ=bigquery.Client()
 ERR_CLIENT=error_reporting.Client()
 
-with open("./config.json") as config:
-    data=json.load(config)
+with open("./config.json") as configFile:
+    data=json.load(configFile)
     BQ_DATASET=data["dataSetName"]
 
 
 def streamCsvFilesFromCsToBq(data,context):
 
-    bucket_name=data["bucket"]
-    file_name=data["name"]
-    dataset_ref=BQ.dataset(BQ_DATASET)
+    bucket_name = data["bucket"]
+    file_name = data["name"]
+    bqTable = getBqTableName(file_name)
+    dataset_ref = BQ.dataset(BQ_DATASET)
 
-    job_config=bigquery.LoadJobConfig(autodetect=True,
-                                      max_bad_records=20,
+    job_config=bigquery.LoadJobConfig(max_bad_records=20,
                                       write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
                                       source_format=bigquery.SourceFormat.CSV,
                                       skip_leading_rows=1)
 
+    job_config.schema = getBqSchema(bqTable)
+
     uri="gs://"+bucket_name+"/"+file_name
-    bqTable=getBqTableName(file_name)
 
     try:
         load_job=BQ.load_table_from_uri(uri,
@@ -33,6 +34,7 @@ def streamCsvFilesFromCsToBq(data,context):
                                         job_config=job_config)
 
         print("Starting job with id: {}".format(load_job.job_id))
+
         print("file {}".format(data["name"]))
 
         load_job.result()
@@ -55,6 +57,17 @@ def getBqTableName(file_name):
             word_list_for_table.append(value.lower())
 
     return "_".join(word_list_for_table)
+
+
+def getBqSchema(bqTable):
+
+    with open("./schema.json") as schemaFile:
+        schemaData=json.load(schemaFile)
+        schemaConfig = []
+        for column in schemaData[bqTable]:
+            schemaConfig.append(bigquery.SchemaField(column["name"], column["type"], mode=column["mode"]))
+
+    return schemaConfig
 
 
 
